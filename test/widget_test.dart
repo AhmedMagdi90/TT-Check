2b5ccef03c2d86302eefd71a5e153556caa9df70
AhmedMagdi90 <ahmedmagdi61@gmail.com>
2026-04-04 11:45:05 +0200

    Add GitHub Actions APK build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tt_check/main.dart';
+import 'package:hive/hive.dart';
+import 'package:tt_check/src/app.dart';
+import 'package:tt_check/src/storage/storage.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  late Directory hiveDirectory;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    hiveDirectory = await Directory.systemTemp.createTemp('tt_check_test_');
+    Hive.init(hiveDirectory.path);
+    await Storage.openBoxes();
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  tearDownAll(() async {
+    await Hive.close();
+    if (await hiveDirectory.exists()) {
+      await hiveDirectory.delete(recursive: true);
+    }
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('app shows primary navigation tabs', (WidgetTester tester) async {
+    await tester.pumpWidget(const TTCheckApp());
+    await tester.pumpAndSettle();
+
+    expect(find.text('Dashboard'), findsOneWidget);
+    expect(find.text('Compare'), findsOneWidget);
+    expect(find.text('Upload'), findsOneWidget);
   });
 }
